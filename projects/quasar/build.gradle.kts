4515944539164a336790d7ff3157127c46291666
XWasHere <xwashere0@protonmail.com>
2026-07-08 10:14:52 -0400

    ADD(Quasar): Set up data/resource pack builds

--- projects/quasar/build.gradle.kts
+++ projects/quasar/build.gradle.kts
@@ -1,8 +1,11 @@
+import com.pswidersk.gradle.python.VenvTask
+
 plugins {
     kotlin("jvm") version "2.4.0"
 
     id("com.gradleup.shadow") version "9.5.1"
     id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("com.pswidersk.python-plugin") version "3.2.17"
 }
 
 repositories {
@@ -17,8 +20,26 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 }
 
+sourceSets {
+    main {
+        kotlin {
+            srcDir("src/main/kotlin/")
+            srcDir("src/generated/kotlin/")
+        }
+
+        resources {
+            srcDir("src/main/generated/")
+            srcDir("src/generated/resources/")
+        }
+    }
+}
+
 kotlin {
     jvmToolchain(25)
+}
+
+pythonPlugin {
+    pythonVersion = "3.10"
 }
 
 tasks {
@@ -26,15 +47,66 @@ tasks {
         dependsOn(shadowJar)
     }
 
+    register("package") {
+        dependsOn("packageServer")
+    }
+
     runServer {
         minecraftVersion("26.2")
     }
 
     processResources {
-        val props = mapOf("version" to version)
+        dependsOn("copyDataPack")
+    }
 
-        filesMatching("plugin.yml") {
-            expand(props)
+    // TODO(XWASHERE): These always run, this can probably be solved with a gradle plugin.
+    register<VenvTask>("configurePython") {
+        venvExec = "pip"
+        args = listOf("install", "smithed")
+    }
+
+    register<VenvTask>("weldDataPack") {
+        dependsOn("configurePython")
+
+        venvExec = "weld"
+        args = listOf("--dir",  "build/weld/data",
+                      "--name", "main.zip",
+                      "src/main/datapack")
+    }
+
+    register<VenvTask>("weldResourcePack") {
+        dependsOn("configurePython")
+
+        venvExec = "weld";
+        args = listOf("--dir",  "build/weld/resources",
+                      "--name", "main.zip",
+                      "src/main/resourcepack")
+    }
+
+    // We can technically skip welding the datapack and copying it into the resources
+    // directory and just keep the entire datapack in resources/custom_datapack, but
+    // in the future we may need to include something else, so it's nice to keep
+    // this here just in case since it doesn't add any overhead.
+    register<Copy>("copyDataPack") {
+        dependsOn("weldDataPack")
+
+        from(zipTree("build/weld/data/main.zip"))
+        into("src/generated/resources/custom_datapack")
+    }
+
+    register<Zip>("packageServer") {
+        dependsOn(build)
+        dependsOn("weldResourcePack")
+
+        from("build/libs/quasar.jar") {
+            into("plugins/")
         }
+
+        from("build/weld/resources/main.zip") {
+            rename { "resources.zip" }
+        }
+
+        archiveFileName = "quasar-server.zip"
+        destinationDirectory = file("build/package")
     }
 }
