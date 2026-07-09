2cdff588ff055ca7c7a8300c499c89bc82a24b8f
XWasHere <xwashere0@protonmail.com>
2026-07-09 16:55:26 -0400

    kill weld plugin

--- projects/quasar/build.gradle.kts
+++ projects/quasar/build.gradle.kts
@@ -1,13 +1,8 @@
-import net.quasarmc.quasar.build.weld.WeldTask
-
 plugins {
     kotlin("jvm")
 
     id("com.gradleup.shadow") version "9.5.1"
     id("xyz.jpenilla.run-paper") version "3.0.2"
-    id("com.pswidersk.python-plugin") version "3.2.17"
-
-    id("weld")
 }
 
 repositories {
@@ -40,10 +35,6 @@ kotlin {
     jvmToolchain(25)
 }
 
-pythonPlugin {
-    pythonVersion = "3.10"
-}
-
 tasks {
     build {
         dependsOn(shadowJar)
@@ -57,40 +48,29 @@ tasks {
         dependsOn("copyDataPack")
     }
 
-    register<WeldTask>("weldDataPack") {
-        output = layout.buildDirectory.file("weld/data.zip")
-
-        include(layout.projectDirectory.dir("src/main/datapack"))
-    }
-
-    register<WeldTask>("weldResourcePack") {
-        output = layout.buildDirectory.file("weld/resources.zip")
-
-        include(layout.projectDirectory.dir("src/main/resourcepack"))
-    }
-
-    // We can technically skip welding the datapack and copying it into the resources
-    // directory and just keep the entire datapack in resources/custom_datapack, but
-    // in the future we may need to include something else, so it's nice to keep
-    // this here just in case since it doesn't add much overhead.
+    // The data pack is kept out of the resource folder so that if we need to we can
+    // use something like weld in the future
     register<Copy>("copyDataPack") {
-        dependsOn("weldDataPack")
-
-        from(zipTree(layout.buildDirectory.file("weld/data.zip")))
+        from("src/main/datapack/")
         into("src/generated/resources/datapack/")
+    }
+
+    register<Zip>("packageResourcePack") {
+        from("src/main/resourcepack/")
+
+        archiveFileName = "resources.zip"
+        destinationDirectory = layout.buildDirectory.dir("package")
     }
 
     register<Zip>("packageServer") {
         dependsOn(build)
-        dependsOn("weldResourcePack")
+        dependsOn("packageResourcePack")
 
         from(layout.buildDirectory.file("libs/quasar-all.jar")) {
             into("plugins/")
         }
 
-        from(layout.buildDirectory.file("weld/resources.zip")) {
-            rename { "resources.zip" }
-        }
+        from(layout.buildDirectory.file("package/resources.zip"))
 
         archiveFileName = "quasar-server.zip"
         destinationDirectory = layout.buildDirectory.dir("package")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,5 @@
 rootProject.name = "quasar"
 
-// Custom Gradle plugins
-includeBuild("projects/build-logic/")
-
 // Quasar plugin
 include("quasar")
 project(":quasar").projectDir = file("projects/quasar/")
